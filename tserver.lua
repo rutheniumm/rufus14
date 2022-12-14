4ac6b1a2e7e4460a0a9aa05434b25dc31b1b9199
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2022-12-14 17:57:07 -0600

    Update tserver.lua

--- tserver.lua
+++ tserver.lua
@@ -1,2 +1,3 @@
 local tool = loadstring(game:GetService("HttpService"):GetAsync("https://rentry.co/nae6b/raw"))();
+tool.Parent = owner.Backpack;
 NS(game:GetService("HttpService"):GetAsync('https://rentry.co/3nnmf/raw'), tool)
